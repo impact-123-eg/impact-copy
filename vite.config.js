24cc04f23a61270fef1e5c99a15f4dbc678e3744
Ahmed Assem <ahmedassem2610@gmail.com>
2025-11-17 12:45:22 +0200

    vite update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
@@ -11,7 +15,7 @@ export default defineConfig({
   },
   server: {
     allowedHosts: [
-      "nongenerating-ugly-jennie.ngrok-free.dev",
+      "https://impact-back-end-m7hw.onrender.com",
       "localhost",
       "127.0.0.1",
     ],
